a7ceb8064fd2c91d43ad4e4f03f1dd854cc74b8e
Snoopy20111 <snoopy20111@gmail.com>
2025-11-05 21:54:25 -0600

    Prt3

--- init.cpp
+++ init.cpp
@@ -1,20 +1,66 @@
 //Main header file for the XDK
 #include <xtl.h>
 
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW|D3DFVF_DIFFUSE)
+
 LPDIRECT3D8 g_pD3D = NULL;                      // DirectX Object
 LPDIRECT3DDEVICE8 g_pD3DDevice = NULL;          // Screen Object
+LPDIRECT3DVERTEXBUFFER8 g_pVertexBuffer = NULL; // Vertices Buffer
+
+struct CUSTOMVERTEX
+{
+    FLOAT x, y, z, rhw; // The transformed position for the vertex.
+    DWORD colour;       // The vertex colour.
+};
+
+static void DrawTriangle()
+{
+    VOID* pVertices = NULL;
+
+    // Store each point of the triangle together with it's colour
+    CUSTOMVERTEX cvVertices[] =
+    {
+        {250.0f, 100.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(255, 0, 0),}, // Vertex 1 - Red (250, 100)
+        {400.0f, 350.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(0, 255, 0),}, // Vertex 2 - Green (400, 350)
+        {100.0f, 350.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(0, 0, 255),}, // Vertex 3 - Blue (100, 350)
+    };
+
+    // Create the vertex buffer from our device
+    g_pD3DDevice->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX),
+        0,
+        D3DFVF_CUSTOMVERTEX,
+        D3DPOOL_DEFAULT,
+        &g_pVertexBuffer);
+
+    // Get a pointer to the vertex buffer vertices and lock the vertex buffer
+    g_pVertexBuffer->Lock(0, sizeof(cvVertices), (BYTE**)&pVertices, 0);
+
+    // Copy our stored vertices values into the vertex buffer
+    memcpy(pVertices, cvVertices, sizeof(cvVertices));
+
+    // Unlock the vertex buffer
+    g_pVertexBuffer->Unlock();
+
+    // Rendering our triangle
+    g_pD3DDevice->SetStreamSource(0, g_pVertexBuffer, sizeof(CUSTOMVERTEX));
+    g_pD3DDevice->SetVertexShader(D3DFVF_CUSTOMVERTEX);
+    g_pD3DDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+
+    // Every time we create a vertex buffer, we must release one!.
+    g_pVertexBuffer->Release();
+}
 
 static void InitialiseD3D()
 {
-    //First of all, create the main D3D object. If it is created successfully we
-    //should get a pointer to an IDirect3D8 interface.
+    // First of all, create the main D3D object. If it is created successfully we
+    // should get a pointer to an IDirect3D8 interface.
     g_pD3D = Direct3DCreate8(D3D_SDK_VERSION);
 
-    //Create a structure to hold the settings for our device
+    // Create a structure to hold the settings for our device
     D3DPRESENT_PARAMETERS d3dpp;
     ZeroMemory(&d3dpp, sizeof(d3dpp));
 
-    //Fill the structure.
+    // Fill the structure.
     // Set fullscreen 640x480x32 mode
     d3dpp.BackBufferWidth = 640;
     d3dpp.BackBufferHeight = 480;
@@ -26,7 +72,7 @@ static void InitialiseD3D()
     // Set up how the backbuffer is "presented" to the frontbuffer each time
     d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
 
-    //Create a Direct3D device.
+    // Create a Direct3D device.
     g_pD3D->CreateDevice(0, D3DDEVTYPE_HAL, NULL, 
         D3DCREATE_HARDWARE_VERTEXPROCESSING,
         &d3dpp, &g_pD3DDevice);
@@ -38,26 +84,27 @@ static void CleanUpD3D()
     g_pD3D->Release();
 }
 
-//Application entry point
+// Application entry point
 void __cdecl main()
 {
     InitialiseD3D();
     while (true)
     {
-        //Clear the backbuffer to blue
+        // Clear the backbuffer to blue
         //                                                          r   g   b
         g_pD3DDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
 
-        //Begin the scene
+        // Begin the scene
         g_pD3DDevice->BeginScene();
 
-        //Draw stuff here
+        // Draw stuff here
+        DrawTriangle();
 
-        //End the scene
+        // End the scene
         g_pD3DDevice->EndScene();
 
-        //Filp the back and front buffers so that whatever has been rendered on the back buffer
-        //will now be visible on screen (front buffer).
+        // Flip the back and front buffers so that whatever has been rendered on the back buffer
+        // will now be visible on screen (front buffer).
         g_pD3DDevice->Present(NULL, NULL, NULL, NULL);
     }
     CleanUpD3D();
