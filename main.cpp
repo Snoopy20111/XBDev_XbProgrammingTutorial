c23c17036658b6ecbb8eb2679e2b49f43c1f6d87
Snoopy20111 <snoopy20111@gmail.com>
2025-11-16 17:57:14 -0600

    Prt8 (Controller Basics)

--- main.cpp
+++ main.cpp
@@ -1,213 +1,78 @@
-#include "init.h"
+//Main header file for the XDK
+#include <xtl.h>
 
-#include <stdio.h>
-#include <fstream>
-
-// Here are a couple of structures to store our data, its so much tidier to use
-// structures, as we can store an XYZ value in the stXYZ stucture, an if we need
-// to access it we can just go stXYZ myP;  myP.x = 0;  etc.
-struct stXYZ
+// Does what it says on the tin.
+// For homebrew stuff, this may not be instantly obvious that it's working
+static void Reboot()
 {
-    float x, y, z;
-};
-
-struct stFace
-{
-    int p1, p2, p3;
-};
-
-struct CUSTOMVERTEX
-{
-    FLOAT x, y, z;
-    DWORD color;
-};
-
-// Global variables.  These will hold the data that contains our shapes.
-stXYZ* pVerts;
-stFace* pFaces;
-int iNum_vectors = 0;
-int iNum_polys = 0;
-
-// Loader function. Should work on pretty much anything, because it only depends
-// on the standard libraries.
-static int loadshape(char* filename)
-{
-    long int color;
-    int i;
-    char temp[16];
-
-    // Temp var used to determine if the polygon is made up of 3 sides (triangle or more)
-    // Error if iNum_vectors = 0 when returning from function
-    int iNum_points = 0; 
-
-    FILE* fin = fopen(filename, "r");
-
-    if (!fin) { return 0; } // If we're here, an error occured opening the file.
-
-    // Read in number of points & polygons in object
-    if (fscanf(fin, "%15s %d %d", temp, &iNum_vectors, &iNum_polys) != 3)
-    {
-        fclose(fin);
-        return 0;
-    }
-
-    // Initialize our arrays
-    pVerts = new stXYZ[iNum_vectors];
-    pFaces = new stFace[iNum_polys];
-
-    // Read in all the vectors used in this object
-    // Also includes a little error checking
-    for (i = 0; i < iNum_vectors; i++)
-    {
-        if (fscanf(fin, "%f %f %f", &pVerts[i].x, &pVerts[i].y, &pVerts[i].z) != 3)
-        {
-            fclose(fin);
-            return 0;
-        }
-    }
-
-    // Read in all of the polygon data
-    for (i = 0; i < iNum_polys; i++)
-    {
-        // We could use the color in some way here, but we'll just set all the faces to the same color for simplicity.
-        // Also includes a little error checking
-        if (fscanf(fin, "%ld", &color) != 1)
-        {
-            fclose(fin);
-            return 0;
-        }
-        if (fscanf(fin, "%ld", &iNum_points) != 1)
-        {
-            fclose(fin);
-            return 0;
-        }
-        if (iNum_points > 3)    // Our code only supports triangles
-        {
-            fclose(fin);
-            return 0;
-        }
-        if (fscanf(fin, "%d %d %d", &pFaces[i].p1, &pFaces[i].p2, &pFaces[i].p3) != 3)
-        {
-            fclose(fin);
-            return 0;
-        }
-    }
-    fclose(fin);
-    return 1;
+	LD_LAUNCH_DASHBOARD LaunchData = { XLD_LAUNCH_DASHBOARD_MAIN_MENU };
+	XLaunchNewImage(NULL, (LAUNCH_DATA*)&LaunchData);
 }
 
-// Up until now, there's been no DirectX. Just StdLibs and the 3d file.
-// For demonstration, we'll just load the vertex points into DirectX buffers
-// and display it; very inefficient but easy to follow.
-static void DrawShape()
+static bool Gamepad()
 {
+	XINPUT_GAMEPAD myGamePad{};
 
-    CUSTOMVERTEX* myVertices = new CUSTOMVERTEX[iNum_polys * 3];
-    // Total number of triangles is iNum_polys
-    int index = 0;
-    for (int i = 0; i < iNum_polys; i++)
+    // What XINPUT_GAMEPAD looks like:
+    /*
+        struct
+        {
+                WORD    wButtons;
+                BYTE    bAnalogButtons[8];
+                SHORT   sThumbLX;
+                SHORT   sThumbLY;
+                SHORT   sThumbRX;
+                SHORT   sThumbRY;
+        } XINPUT_GAMEPAD;
+    */
+
+    DWORD dwInsertions, dwRemovals;
+    XGetDeviceChanges(XDEVICE_TYPE_GAMEPAD, &dwInsertions, &dwRemovals);
+
+    static HANDLE pGamePd;
+    // dwInsertion contains the 'bitwise' information of currently inserted gamepads
+     //
+     //     etc
+     //         Gamepad 2
+     //         | Gamepad 1
+     //         | | Gamepad 0
+     //         | | |
+     // 0 0 ... 0 0 1
+
+    // If there's a controller in the first slot (farthest left), grab its input
+    if (dwInsertions & 1)
     {
-        myVertices[index].x = pVerts[pFaces[i].p1].x;
-        myVertices[index].y = pVerts[pFaces[i].p1].y;
-        myVertices[index].z = pVerts[pFaces[i].p1].z;
-        myVertices[index].color = 0xff00ffff;
-        index++;
-
-        myVertices[index].x = pVerts[pFaces[i].p2].x;
-        myVertices[index].y = pVerts[pFaces[i].p2].y;
-        myVertices[index].z = pVerts[pFaces[i].p2].z;
-        myVertices[index].color = 0xff0ff00f;
-        index++;
-
-        myVertices[index].x = pVerts[pFaces[i].p3].x;
-        myVertices[index].y = pVerts[pFaces[i].p3].y;
-        myVertices[index].z = pVerts[pFaces[i].p3].z;
-        myVertices[index].color = 0xff00ffff;
-        index++;
+        pGamePd = XInputOpen(XDEVICE_TYPE_GAMEPAD, 0, XDEVICE_NO_SLOT, NULL);
     }
 
-    LPDIRECT3DVERTEXBUFFER8 pVertexBuffer = NULL;
-    UINT D3DFVF_CUSTOMVERTEX = (D3DFVF_XYZ | D3DFVF_DIFFUSE);
+    // If we have input, get the data from that gamepad
+    if (pGamePd)
+    {
+        XINPUT_STATE myInputStates;
+        XInputGetState(pGamePd, &myInputStates);
+        memcpy(&myGamePad, &myInputStates.Gamepad, sizeof(XINPUT_GAMEPAD));
+    }
 
-    g_pD3DDevice->CreateVertexBuffer(3 * iNum_polys * sizeof(CUSTOMVERTEX),
-        0, D3DFVF_CUSTOMVERTEX,
-        D3DPOOL_DEFAULT, &pVertexBuffer);
-    VOID* pV = nullptr;
-    pVertexBuffer->Lock(0, 0, (BYTE**)&pV, 0);
-    memcpy(pV, myVertices, sizeof(CUSTOMVERTEX) * iNum_polys * 3);
-    pVertexBuffer->Unlock();
-
-    g_pD3DDevice->SetStreamSource(0, pVertexBuffer, sizeof(CUSTOMVERTEX));
-    g_pD3DDevice->SetVertexShader(D3DFVF_CUSTOMVERTEX);
-    g_pD3DDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, iNum_polys);
-
-    pVertexBuffer->Release();
-    delete[] myVertices;
-}
-
-static void Render()
-{
-    // Clear the backbuffer to blue, and clear the Z Buffer
-    //                                                                             r  g   b
-    g_pD3DDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
-
-    // Begin the scene
-    g_pD3DDevice->BeginScene();
-
-    // Camera. DirectX handles a lot of the matrix math here.
-    D3DXMATRIX view_matrix, matProj;
-
-    D3DXMatrixLookAtLH(&view_matrix,        // Result Matrix
-        &D3DXVECTOR3(0.0f, 0.0f, -5.0f),    // "Eye" (camera) location
-        &D3DXVECTOR3(0.0f, 0.0f, 0.0f),     // "Look At" location
-        &D3DXVECTOR3(0.0f, 1.0f, 0.0f)      // "Up" direction
-    );
-    g_pD3DDevice->SetTransform(D3DTS_VIEW, &view_matrix);
-
-    D3DXMatrixPerspectiveFovLH(&matProj,    // Result Matrix
-        D3DX_PI / 3,                        // Field of View, in radians (60°)
-        (600.0f / 400.0f),                  // Aspect ratio
-        1.0f,                               // Near view plane
-        1000.0f                             // Far view plane
-    );
-    g_pD3DDevice->SetTransform(D3DTS_PROJECTION, &matProj);
-    // End Camera
-
-    // Scary matrix math, but basically it rotates our shape
-    D3DXMATRIX matWorld;
-    D3DXMATRIX trans_matrix;
-    D3DXMATRIX y_rot_matrix;
-    static float y_rot = 0;
-    y_rot -= 0.005f;
-    //Set up the rotation matrix
-    D3DXMatrixRotationY(&y_rot_matrix, y_rot);
-    //Set up the translation matrix (move it over to the left a bit)
-    D3DXMatrixTranslation(&trans_matrix, -1.0f, 0.0f, 0.0f);
-    //Combine out matrices (basically making the rotation and translation one "action")
-    D3DXMatrixMultiply(&matWorld, &y_rot_matrix, &trans_matrix);
-    //Set our World Matrix (apply the adjustments above)
-    g_pD3DDevice->SetTransform(D3DTS_WORLD, &matWorld);
-
-    DrawShape();
-
-    // End the scene
-    g_pD3DDevice->EndScene();
-
-    // Flip the back and front buffers so that whatever has been rendered on the back buffer
-    // will now be visible on screen (front buffer).
-    g_pD3DDevice->Present(NULL, NULL, NULL, NULL);
+    // Press the gamepad up button, and the function returns true, else false
+    if (myGamePad.wButtons & XINPUT_GAMEPAD_DPAD_UP)
+    {
+        return true;
+    }
+    return false;
 }
 
 // Application entry point
 void __cdecl main()
 {
-    InitialiseD3D();
+    XInitDevices(0, 0);
 
-    loadshape("D:\\Shape.model");
-
+    // We keep checking the gamepad until the up D-Pad button is pressed
+    // When that's true, leave the loop and reboot the Xbox!
     while (true)
     {
-        Render();
+        bool pressedUp = Gamepad();
+        if (pressedUp) { break; }
     }
-    CleanUpD3D();
+
+    Reboot();
 }
